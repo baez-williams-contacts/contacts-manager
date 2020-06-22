d71770923894c4c2b9fb96db929f8a321279dcfa
JahzielB <jahzielb@gmail.com>
2020-06-22 15:29:57 -0500

    created methods in the contact class and created ContactApps and contacts.txt

--- src/Contact.java
+++ src/Contact.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Contact {
     private String name;
     private int phoneNumber;
@@ -21,5 +24,23 @@ public class Contact {
     public Contact(String name, int phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
+    }
+
+    public static List<String> contactsToNameStrings(List<Contact> contacts) {
+        List<String> contactInfo = new ArrayList<>();
+        for (Contact contact : contacts) {
+            String contactString = contact.getName() + " | " + contact.getPhoneNumber();
+            contactInfo.add(contactString);
+        }
+        return contactInfo;
+    }
+
+    public static List<Contact> nameStringsToContacts(List<String> contactInfo) {
+        List<Contact> contactObj = new ArrayList<>();
+        for (String contact : contactInfo) {
+            String[] contactArr = contact.split("|", 2);
+            contactObj.add(new Contact(contactArr[0], Integer.parseInt(contactArr[1])));
+        }
+        return contactObj;
     }
 }
